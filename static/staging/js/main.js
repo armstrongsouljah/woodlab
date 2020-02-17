a0a83fbb7bb8974cb1b0e1e8a51a13159c48b055
Armstrong <armstrongsouljah@gmail.com>
2020-02-18 00:02:17 +0300

    feat(menu): Implement scrolling menu
    - Add navigation menu
    - Enable scrolling to page section on menu select
    - Refine footer look

--- static/staging/js/main.js
+++ static/staging/js/main.js
@@ -2,3 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+  });
